6fc70324a4aa22a60c84352b57b253e4d90123b3
zukui <71074389+zukui1984@users.noreply.github.com>
2020-11-25 20:35:34 +0100

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,12 @@ let pokemonRepository = (function () {
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
+    let listPokemon = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
     button.addEventListener("click", function () {
       showDetails(pokemon);
     });
